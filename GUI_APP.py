7195a2da99175ec0df75abe2376e1ca6b7672b18
anas khan <anaskhan93511@gmail.com>
2022-10-10 15:46:01 +0530

    anas

--- GUI_APP.py
+++ GUI_APP.py
@@ -1,7 +1,7 @@
-# GUI - graphical user interface 
+# GUI - graphical user interface
 
-# libraries 
-# 1. tkinter 
+# libraries
+# 1. tkinter
 # 2. pYQt
 # 3. turtle
 
@@ -10,10 +10,60 @@ app = ttk.Tk()
 app.title("my app")
 app.geometry('600x400')
 
-ttk.Label(app, text = 'a simple Text Lable'). place(x=50 , y=50)
-ttk.Label (app, text  = 'anas khan'  ).place(x= 20 , y=20)
-ttk.Label (app, text  = 'awasthi'  ).place(x= 10 , y=70)
+msg = ttk.Variable(app)
+result = ttk.Variable(app)
+# print(msg.get())
+# msg.set('empty')
+# print(msg.get())
 
+f1 = ttk.Variable(app)
+f1. set('0')
+f2 = ttk.Variable(app)
+f2.set('0')
+
+#ttk.Label(app, text = 'a simple Text Lable'). place(x=50 , y=50)
+#ttk.Label(app , textvariable=msg,font='algerian' ).place(x=100 , y= 70)
+
+
+ttk.Entry(app,  textvariable=f1, font=('Arial', 22)).place(x=50, y=200)
+ttk.Entry(app, textvariable=f2, font=('Arial', 22)).place(x=150, y=200)
+
+ttk.Label(app, text='Result is:').place(x=100, y=300)
+ttk.Label(app, textvariable=result, font=('arial', 22)).place(x=100, y=330)
+# def abc ():
+#   print ('wow')
+# msg.set('jp tera man kare ')
+
+# ttk.Button(app, text = 'isko click krdo' , command  = abc ).place(x= 100 , y=100)
+
+
+# ttk.Button(app, text = 'ye wala bhi h ' , command  = lambda:msg.set('pta nhi')).place(x= 100 , y=130)
+
+
+def calci(op):
+    print('i will calculate')
+    result.set(eval(f1.get()+op+f2.get()))
+
+
+ttk.Button(app, text='+', command=lambda: calci('+'),
+           font=('Arial', 22)).place(x=50, y=240)
+ttk.Button(app, text='-', command=lambda: calci('-'),
+           font=('Arial', 22)).place(x=100, y=240)
+ttk.Button(app, text='*', command=lambda: calci('*'),
+           font=('Arial', 22)).place(x=150, y=240)
+ttk.Button(app, text='/', command=lambda: calci('/'),
+           font=('Arial', 22)).place(x=200, y=240)
+
+box = ttk.Listbox(app , height= 5, fg = 'red', activestyle='dotbox')
+box.insert(1, 'sample1')
+box.insert(2, 'sample2')
+box.insert(3, 'sample3')
+box.place(x=350 , y=40)
+
+def show():
+    print(box.get(box.curselection()))
+
+ttk.Button(app , text = 'show' , command=show).place(x=350 , y= 250)
 
 
 app.mainloop()
---
+++ GUI_movie.py
@@ -0,0 +1,39 @@
+from numpy import place
+import tkinter as ttk
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+
+app = ttk.Tk()
+app.title('Movie Recommendation')
+app.geometry('700x700')
+
+cols = ['user_id', 'movie_id', 'rating', 'ts']
+df = pd.read_csv('u.data', sep='\t', names=cols).drop('ts', axis=1)
+item_cols = ['movie_id', 'title'] + [str(i) for i in range(22)]
+df1 = pd.read_csv('u.item', sep='|', names=item_cols,
+                  encoding="ISO-8859-1")[['movie_id', 'title']]
+movie = pd.merge(df, df1, on='movie_id')
+
+ttk.Label(app, text='Movie Recommendation',
+          font=('Algerian', 18)). place(x=300, y=10)
+result = ttk.Variable(app)
+
+box = ttk.Listbox(app, height=15 , width = 40)
+for row, val  in movie. iterrows():
+    box.insert(row+1 , val['title'])
+
+box.place(x=10, y=10)
+
+
+def get_movie():
+    pass
+
+
+ttk.Button(app, text='Find recommendation', command=get_movie(),
+           font=('Arial', 22)).place(x=50, y=260)
+ttk.Label(app, textvariable=result, font=('arial', 22)).place(x=200, y=200)
+
+
+app.mainloop()
